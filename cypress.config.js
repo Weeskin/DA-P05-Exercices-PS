574586394b27d9de7f454ba69a0c4aa38998284b
Pierre <pierre.sourice38@gmail.com>
2025-02-28 11:22:59 +0100

    Test Ok

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'qi6ou9',
   e2e: {
     baseUrl: 'http://127.0.0.1:8080',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
